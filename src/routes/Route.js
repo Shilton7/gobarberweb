de9e81b6a8b10f52154920d8222f1ce554426c56
Shilton <shilton.sistemas@gmail.com>
2020-03-04 11:06:32 -0300

    Configuring Private Routes

---
+++ src/routes/Route.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  //usuario está logado ou não
+  const signed = false;
+
+  //não logado acessando rota privada
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  //logado
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return <Route {...rest} component={Component} />;
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
