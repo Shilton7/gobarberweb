08696c944268f67ec021f2e5ea1f5b9f7c2baaf4
Shilton <shilton.sistemas@gmail.com>
2020-03-04 15:21:03 -0300

    Validating forms

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import logo from '~/assets/img/logo.svg';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+const validations = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -12,7 +22,7 @@ export default function SignIn() {
     <>
       <img src={logo} alt="logo" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={validations} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
 
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import logo from '~/assets/img/logo.svg';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+const validations = Yup.object().shape({
+  name: Yup.string().required('O campo nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -11,7 +22,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="logo" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={validations} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Seu Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
