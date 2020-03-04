4c24057b1a829196eab9effcc0564d6836558631
Shilton <shilton.sistemas@gmail.com>
2020-03-04 14:08:33 -0300

    Adjusting layout of authentication screens

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import logo from '~/assets/img/logo.svg';
+import { Link } from 'react-router-dom';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <img src={logo} alt="logo" />
+
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import logo from '~/assets/img/logo.svg';
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <img src={logo} alt="logo" />
+
+      <form>
+        <input placeholder="Seu Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
