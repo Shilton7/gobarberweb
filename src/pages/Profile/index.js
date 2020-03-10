02ee050e246390bd040b6461fd84a9f6c637ebf8
Shilton <shilton.sistemas@gmail.com>
2020-03-10 08:47:21 -0300

    Profile form

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { useSelector } from 'react-redux';
+import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const profile = useSelector(state => state.user.profileUser);
+
+  return (
+    <Container>
+      <Form initialData={profile}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+
+        <hr />
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Sua nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirme sua nova senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair</button>
+    </Container>
+  );
 }
