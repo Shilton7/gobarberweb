c726ec7f97f8b10baf80456a792356f78bc380e2
Shilton <shilton.sistemas@gmail.com>
2020-03-12 19:19:27 -0300

    Dashboard styling

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Container, Time } from './styles';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size="36" color="#FFF" />
+        </button>
+
+        <strong> 9 de Agosto</strong>
+
+        <button type="button">
+          <MdChevronRight size="36" color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Letycia</span>
+        </Time>
+
+        <Time available>
+          <strong>09:00</strong>
+          <span>Diego</span>
+        </Time>
+
+        <Time>
+          <strong>10:00</strong>
+          <span>Livre</span>
+        </Time>
+
+        <Time>
+          <strong>11:00</strong>
+          <span>Livre</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
